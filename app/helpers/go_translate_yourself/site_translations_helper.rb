86cbfdc1d4d15302e0a298207096bf408a859c02
Josh Adams <josh@isotope11.com>
2010-12-27 16:03:18 -0600

    make site translations helper pull in default value if it's not defined in the backend.

--- app/helpers/go_translate_yourself/site_translations_helper.rb
+++ app/helpers/go_translate_yourself/site_translations_helper.rb
@@ -4,8 +4,13 @@ module GoTranslateYourself
       key_parts = key.split('.').inject('') do |kp, key_part|
         kp << "[#{key_part}]"
       end
+      translation_text = begin
+        I18n.t!(key, :locale => locale)
+      rescue I18n::ArgumentError
+        GoTranslateYourself.current_store.default_translation("dev.#{key}")[1..-2]
+      end
       text_field_tag("translations[#{locale}]#{key_parts}",
-                     I18n.t(key, :locale => locale) || "",
+                     translation_text || "",
                      :size => (default_translation.size + 20))
     end
   end
